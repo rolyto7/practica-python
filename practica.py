317ce05176b05709700fa7898470f6084456d4fd
Rolylázaro <rolylazaro@gmail.com>
2026-05-27 22:23:15 -0400

    se añadio datos ciudad

--- practica.py
+++ practica.py
@@ -1,4 +1,9 @@
+# solicitar datos  
 nombre = input("Ingrese su nombre: ")
 edad = input("Ingrese su edad: ")
+ciudad = input("Ingrese su ciudad: ")
+
+# imprimir datos 
 print("Nombre:", nombre)
 print("Edad:", edad)
+print("Ciudad:", ciudad)
